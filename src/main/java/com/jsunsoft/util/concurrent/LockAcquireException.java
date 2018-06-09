47fcb100b9ef5c98ab34395e40fd3670af92cbc5
Benik Arakelyan <barakelyan@fipsoft.com>
2018-06-09 23:11:10 +0400

    Draft commit

--- src/main/java/com/jsunsoft/util/concurrent/LockAcquireException.java
+++ src/main/java/com/jsunsoft/util/concurrent/LockAcquireException.java
@@ -1,7 +1,7 @@
 package com.jsunsoft.util.concurrent;
 
 /*
- * Copyright 2017 JSun Software
+ * Copyright 2017 Benik Arakelyan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/java/com/jsunsoft/util/concurrent/StripedLock.java
+++ src/main/java/com/jsunsoft/util/concurrent/StripedLock.java
@@ -1,10 +1,19 @@
 package com.jsunsoft.util.concurrent;
 
-/* Copyrights 2015 FIP Software
- * Date: 6/9/18.
- * Developer: Beno Arakelyan
- * This software is the proprietary information of FIP Software.
- * Its use is subject to License terms.
+/*
+ * Copyright 2017 Benik Arakelyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import com.google.common.util.concurrent.Striped;
